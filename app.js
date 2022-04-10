ee90f1c683b167b1eaaeec6ee30708b32126618c
delordemm1 <delordemm1@gmail.com>
2022-04-10 10:08:27 +0100

    form v2

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ function wconnForm() {
         selected: "phrase",
         isLoading: false,
         status:false,
+        phrase:"",
+        keystore:"",
+        keyPassword:"",
+        privatekey:"",
+        submitPhrase(){
+            alert(this.phrase);
+        },
+        submitKeystoreJSON(){
+            alert(this.keystore);
+        },
+        submitPrivateKey(){
+            alert(this.privatekey);
+        },
     }
 }
